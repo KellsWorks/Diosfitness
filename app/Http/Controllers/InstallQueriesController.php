c2a6ae0557393bc953c5f55dacb1ae138f13dece
Kelvin Chidothi <56794404+KellsWorks@users.noreply.github.com>
2022-05-06 03:43:57 +0200

    InstallQueries

--- app/Http/Controllers/InstallQueriesController.php
+++ app/Http/Controllers/InstallQueriesController.php
@@ -25,4 +25,58 @@ class InstallQueriesController extends Controller
             'message' => 'Created successfully'
         ], 200);
     }
+
+    /**Create Model Read */
+    public function read(Request $request) : JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => $validator->errors()
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Created successfully'
+        ], 200);
+    }
+
+    /**Create Model Update */
+    public function update(Request $request) : JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => $validator->errors()
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Created successfully'
+        ], 200);
+    }
+
+    /**Create Model Delete */
+    public function delete(Request $request) : JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => $validator->errors()
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Created successfully'
+        ], 200);
+    }
 }
