635f8e3477ee804293f350f1e6e934c2295cb216
Kelvin Chidothi <56794404+KellsWorks@users.noreply.github.com>
2022-05-22 07:08:49 +0200

    Customers

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomersController extends Controller
 {
     public function create(Request $request) : JsonResponse
     {
+        $validator = Validator::make($request->all, [
+            'username' => 'required|string',
+            'email' => 'required|email',
+            'address1' => 'required|string',
+            'address2' => 'required|string'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => $validator->errors()
+            ]);
+        }
+
+        Customers::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'address1' => $request->address1,
+            'address2' => $request->address2
+        ]);
+
         return response()->json([
             'message' => 'Customer added successfully'
         ], 200);
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('customers')->group( function() {
+
+    Route::post('create', [CustomersController::class, 'create']);
+    
 });
